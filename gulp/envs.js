d857a8f9fd024892550f582bb251e8e4a4386b71
Daniel Wilkinson <daniel.wilkinson@wipro.com>
2019-02-28 10:53:24 +0000

    feat: initial shell structure

---
+++ gulp/envs.js
@@ -0,0 +1,31 @@
+const minimist = require("minimist");
+const envs = require("../config/envs.json");
+const availableEnvs = Object.keys(envs);
+const defaultEnv = availableEnvs[0];
+
+const knownOptions = {
+  string: "env",
+  default: { env: defaultEnv }
+};
+
+const options = minimist(process.argv.slice(2), knownOptions);
+
+module.exports = {
+  availableEnvs,
+
+  defaultEnv,
+
+  currentEnv: options.env,
+
+  shouldOptimise: function() {
+    return this.getCurrentEnvInfo().optimise || false;
+  },
+
+  getEnvInfo: function(env) {
+    return envs[env];
+  },
+
+  getCurrentEnvInfo: function() {
+    return envs[options.env];
+  }
+};
---
+++ src/scripts/main.js
@@ -0,0 +1,1 @@
+console.log("Metalsmith with gravity scripts loaded.");
