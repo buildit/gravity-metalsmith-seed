855e379b1841938497979dd58e4d941fc70f18ad
Daniel Wilkinson <daniel.wilkinson@wipro.com>
2019-03-12 13:03:17 +0000

    feat: example content page and header& footer

--- gulpfile.js
+++ gulpfile.js
@@ -136,7 +136,7 @@ function watch(done) {
   gulp.watch(paths.images.src, gulp.series(imageOptim, browserSync.reload));
   gulp.watch(paths.assets.src, gulp.series(assets, browserSync.reload));
   gulp.watch(
-    [paths.pages.src, paths.layouts.src, paths.configs.src, "metalsmith.json"],
+    [paths.pages.src, paths.layouts.src, paths.configs.src, paths.content.src, "metalsmith.json"],
     gulp.series(
       "clean",
       metalsmithBuild,
